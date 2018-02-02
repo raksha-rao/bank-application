8a05ada04ce84ab72acdb7facc9c76148513b1d2
Raksha Rao <raksharao@Rakshas-MacBook-Air.local>
2018-02-02 21:11:51 +0530

    test commit

--- src/test/java/com/retry/test/AnnotationTransformer.java
+++ src/test/java/com/retry/test/AnnotationTransformer.java
@@ -12,4 +12,5 @@ public class AnnotationTransformer implements IAnnotationTransformer {
     public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
         annotation.setRetryAnalyzer(RetryAnalyzer.class);
     }
+
 }
